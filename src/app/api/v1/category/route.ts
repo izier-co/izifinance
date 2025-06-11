59ab63360f26fc305c9784ae5d3237eeffeb5a60
testboiz <tvmini0202@gmail.com>
2025-06-11 16:32:51 +0700

    feat (api): support for soft deletion

--- src/app/api/v1/category/route.ts
+++ src/app/api/v1/category/route.ts
@@ -20,7 +20,9 @@ export const GET = async (req: NextRequest) => {
   const { data, error } = await supabase
     .from("m_category")
     .select("*")
-    .range((page - 1) * 100, page * 100);
+    .range((page - 1) * 100, page * 100)
+    .eq("boActive", true)
+    .eq("boStatus", true);
 
   if (error)
     return NextResponse.json({ error: error.message }, { status: 500 });
@@ -48,11 +50,37 @@ export const POST = async (req: NextRequest) => {
     );
   }
 
-  const { error } = await supabase
+  const { data, error } = await supabase
     .from("m_category")
     .insert(categoryModel.data);
 
   if (error)
     return NextResponse.json({ error: error.message }, { status: 500 });
-  return NextResponse.json({ message: "Data Successfully Inserted!" });
+  return NextResponse.json({
+    message: "Data Successfully Inserted!",
+    data: data,
+  });
+};
+
+export const DELETE = async (req: NextRequest) => {
+  const params = req.nextUrl.searchParams;
+  const idParam = params.get("id");
+  if (idParam === null)
+    return NextResponse.json(
+      { error: "400 Bad Request : id parameter is required" },
+      { status: 400 }
+    );
+  const id = Number.parseInt(idParam);
+
+  const { error } = await supabase
+    .from("m_category")
+    .update({ boActive: false, boStatus: false })
+    .eq("inCategoryID", id);
+
+  if (error)
+    return NextResponse.json({ error: error.message }, { status: 500 });
+
+  return NextResponse.json({
+    message: "Data Successfully Deleted!",
+  });
 };
