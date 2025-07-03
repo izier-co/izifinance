e4c555efebad225fed39e0bfb25271c366672034
testboiz <tvmini0202@gmail.com>
2025-07-03 12:39:47 +0700

    feat (middleware) : handle cookies and redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
-import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
+// import { Ratelimit } from "@upstash/ratelimit";
 import { NextRequest, NextResponse } from "next/server";
+import { updateSession } from "./app/api/supabase_middleware.config";
 
-const ratelimiter = new Ratelimit({
-  redis: kv,
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
-});
+// const ratelimiter = new Ratelimit({
+//   redis: kv,
+//   limiter: Ratelimit.slidingWindow(10, "10 s"),
+// });
 
 export async function middleware(req: NextRequest) {
   // const ip = req.headers.get("x-forwarded-for") ?? "127.0.0.1";
@@ -15,9 +15,12 @@ export async function middleware(req: NextRequest) {
   // if (!success) {
   //   return NextResponse.json({ message: "Too many requests" }, { status: 429 });
   // }
+
+  updateSession(req);
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/api/:path*"],
+  matcher: ["/api/((?!v1/auth).*)"],
 };
