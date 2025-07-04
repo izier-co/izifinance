fae1d2a952f409d3570f5375b0dca224fbd9412d
testboiz <tvmini0202@gmail.com>
2025-07-04 11:36:17 +0700

    refactor (test) : split test cases and refactor URL params

--- tests/category/get.test.ts
+++ tests/category/get.test.ts
@@ -15,9 +15,6 @@ beforeEach(() => {
   vitest.clearAllMocks();
 });
 
-const url = "localhost:3000";
-const req = new NextRequest(url);
-
 const MILISECONDS_IN_DAY = 1000 * 60 * 60 * 24;
 const NOW = Date.now();
 const YESTERDAY = new Date(NOW - MILISECONDS_IN_DAY).toISOString();
@@ -25,77 +22,90 @@ const TOMORROW = new Date(NOW + MILISECONDS_IN_DAY).toISOString();
 const DAY_BEFORE_YESTERDAY = new Date(NOW - MILISECONDS_IN_DAY).toISOString();
 const DAY_AFTER_TOMORROW = new Date(NOW + MILISECONDS_IN_DAY).toISOString();
 
-const pageIDStr = "2";
-const paginationSize = "50";
-const fieldsStr = "txCategoryName,txCategoryDescription";
+// const pageIDStr = "2";
+// const paginationSize = "50";
+// const fieldsStr = "txCategoryName,txCategoryDescription";
+
+const urlParams = {
+  pageIDStr: "2",
+  pageIDNum: 2,
+  paginationSizeStr: "50",
+  paginationSizeNum: 50,
+  nameStr: "abc",
+  fieldsStr: "inReimbursementNoteID,inRecipientCompanyCode",
+  isAlphabeticalStr: "true",
+  selectAllStr: "*",
+};
+
+const mockParams = {
+  paginationPage: urlParams.pageIDStr,
+  paginationSize: urlParams.paginationSizeStr,
+  fields: urlParams.fieldsStr,
+  name: urlParams.nameStr,
+  isAlphabetical: urlParams.isAlphabeticalStr,
+  createdBefore: TOMORROW,
+  createdAfter: YESTERDAY,
+  updatedBefore: DAY_AFTER_TOMORROW,
+  updatedAfter: DAY_BEFORE_YESTERDAY,
+};
+
+// question mark for easy concaternation
+const url = "localhost:3000?";
+const req = new NextRequest(url);
+const urlParamString = new URLSearchParams(mockParams).toString();
+const reqWithParams = new NextRequest(url + urlParamString);
 
 // expected to become ISO string in API
 const expectedGtCalls = [
-  ["daCreatedAt", new Date(YESTERDAY).toISOString()],
-  ["daUpdatedAt", new Date(DAY_BEFORE_YESTERDAY).toISOString()],
+  ["daCreatedAt", YESTERDAY],
+  ["daUpdatedAt", DAY_BEFORE_YESTERDAY],
 ];
 
 const expectedLtCalls = [
-  ["daCreatedAt", new Date(TOMORROW).toISOString()],
-  ["daUpdatedAt", new Date(DAY_AFTER_TOMORROW).toISOString()],
+  ["daCreatedAt", TOMORROW],
+  ["daUpdatedAt", DAY_AFTER_TOMORROW],
 ];
-describe("GET /categories tests", () => {
-  test("GET without authorization", async () => {
-    mockSupabase.auth.getSession.mockResolvedValueOnce({
-      data: {
-        session: null,
-      },
-      error: new AuthError("Mock Auth Error"),
-    });
-    const response = await GET(req);
-    const body = await response.json();
-    expect(response.status).toBe(401);
-    expect(body).toEqual({
-      error: "401 Unauthorized",
-    });
-  });
-  test("GET with unregistered user", async () => {
-    mockSupabase.auth.getSession.mockResolvedValueOnce({
-      data: {
-        session: null,
-      },
-      error: null,
-    });
-    const response = await GET(req);
-    const body = await response.json();
-    expect(response.status).toBe(401);
-    expect(body).toEqual({
-      error: "401 Unauthorized",
-    });
-  });
 
+describe("GET /categories successes", () => {
   test("GET without parameters", async () => {
     const response = await GET(req);
     expect(response.status).toBe(200);
   });
 
-  test("GET with all parameters", async () => {
-    const mockSearchParams = req.nextUrl.searchParams;
-    mockSearchParams.append("paginationPage", pageIDStr);
-    mockSearchParams.append("paginationSize", paginationSize);
-    mockSearchParams.append("name", "abc");
-    mockSearchParams.append("isAlphabetical", "true");
-    mockSearchParams.append("fields", fieldsStr);
+  test("GET with url parameters and correct pagination", async () => {
+    const response = await GET(reqWithParams);
+    expect(response.status).toBe(200);
 
-    mockSearchParams.append("createdBefore", TOMORROW);
-    mockSearchParams.append("createdAfter", YESTERDAY);
-    mockSearchParams.append("updatedBefore", DAY_AFTER_TOMORROW);
-    mockSearchParams.append("updatedAfter", DAY_BEFORE_YESTERDAY);
+    const responseData = await response.json();
+    const metadata = responseData["meta"];
 
-    const response = await GET(req);
+    expect(metadata["isFirstPage"]).toBe(urlParams.pageIDNum === 1);
+    expect(metadata["isLastPage"]).toBe(
+      metadata["dataCount"] < urlParams.paginationSizeNum
+    );
+    expect(metadata["dataCount"]).toBe(responseData["data"].length);
+    expect(metadata["totalDataCount"]).toBe(testingGlobalVars.MOCK_COUNT);
+    expect(metadata["pageCount"]).toBe(
+      testingGlobalVars.MOCK_COUNT / urlParams.paginationSizeNum
+    );
+    expect(metadata["offset"]).toBe(
+      (urlParams.pageIDNum - 1) * urlParams.paginationSizeNum
+    );
+    expect(metadata["pageNumber"]).toBe(urlParams.pageIDNum);
+    expect(metadata["paginationSize"]).toBe(urlParams.paginationSizeNum);
+  });
 
-    expect(mockSupabase.select).toHaveBeenCalledWith(fieldsStr, {
+  test("GET lists with correct Supabase function parameters", async () => {
+    const response = await GET(reqWithParams);
+    expect(response.status).toBe(200);
+
+    expect(mockSupabase.select).toHaveBeenCalledWith(urlParams.fieldsStr, {
       count: "exact",
     });
 
     expect(mockSupabase.range).toHaveBeenCalledWith(
-      (Number.parseInt(pageIDStr) - 1) * Number.parseInt(paginationSize),
-      Number.parseInt(pageIDStr) * Number.parseInt(paginationSize)
+      (urlParams.pageIDNum - 1) * urlParams.paginationSizeNum,
+      urlParams.pageIDNum * urlParams.paginationSizeNum
     );
     expect(mockSupabase.order).toHaveBeenCalledWith("txCategoryName", {
       ascending: true,
@@ -128,32 +138,37 @@ describe("GET /categories tests", () => {
       );
       expect(found).toBe(true);
     }
+  });
+});
 
-    const responseData = await response.json();
-    const responseMetadata = responseData["meta"];
-
-    expect(responseMetadata["isFirstPage"]).toBe(
-      Number.parseInt(pageIDStr) === 1
-    );
-    expect(responseMetadata["isLastPage"]).toBe(
-      responseMetadata["dataCount"] < Number.parseInt(paginationSize)
-    );
-    expect(responseMetadata["dataCount"]).toBe(responseData["data"].length);
-    expect(responseMetadata["totalDataCount"]).toBe(
-      testingGlobalVars.MOCK_COUNT
-    );
-    expect(responseMetadata["pageCount"]).toBe(
-      testingGlobalVars.MOCK_COUNT / Number.parseInt(paginationSize)
-    );
-    expect(responseMetadata["offset"]).toBe(
-      (Number.parseInt(pageIDStr) - 1) * Number.parseInt(paginationSize)
-    );
-    expect(responseMetadata["pageNumber"]).toBe(Number.parseInt(pageIDStr));
-    expect(responseMetadata["paginationSize"]).toBe(
-      Number.parseInt(paginationSize)
-    );
-
-    expect(response.status).toBe(200);
+describe("GET /categories failures", () => {
+  test("GET without authorization", async () => {
+    mockSupabase.auth.getSession.mockResolvedValueOnce({
+      data: {
+        session: null,
+      },
+      error: new AuthError("Mock Auth Error"),
+    });
+    const response = await GET(req);
+    const body = await response.json();
+    expect(response.status).toBe(401);
+    expect(body).toEqual({
+      error: "401 Unauthorized",
+    });
+  });
+  test("GET with unregistered user", async () => {
+    mockSupabase.auth.getSession.mockResolvedValueOnce({
+      data: {
+        session: null,
+      },
+      error: null,
+    });
+    const response = await GET(req);
+    const body = await response.json();
+    expect(response.status).toBe(401);
+    expect(body).toEqual({
+      error: "401 Unauthorized",
+    });
   });
 
   test("GET normally but with error in database", async () => {
