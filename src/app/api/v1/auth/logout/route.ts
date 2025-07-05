a54b43048054389244821b9ca060ca2c92688e43
testboiz <tvmini0202@gmail.com>
2025-07-05 11:04:55 +0700

    chore (API) : simplify logout error message

--- src/app/api/v1/auth/logout/route.ts
+++ src/app/api/v1/auth/logout/route.ts
@@ -4,10 +4,7 @@ import { supabase } from "@supabase-config";
 export async function POST() {
   const { error } = await supabase.auth.signOut();
   if (error) {
-    return NextResponse.json(
-      { error: "500 Internal Server Error : " + error.message },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
   return NextResponse.json({ message: "Logged Out Successfully!" });
 }
