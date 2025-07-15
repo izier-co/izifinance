e2b1d34e937a998a3676e72b644d36f6498e48c4
testboiz <tvmini0202@gmail.com>
2025-07-15 17:06:02 +0700

    fix (API) : wrap login flow with try catch

--- src/app/api/v1/auth/signin/route.ts
+++ src/app/api/v1/auth/signin/route.ts
@@ -2,27 +2,31 @@ import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@/app/api/supabase_server.config";
 
 export async function POST(req: NextRequest) {
-  const { email, password } = await req.json();
-  const supabase = await createClient();
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+  try {
+    const { email, password } = await req.json();
 
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-
-  const { session } = data;
-
-  if (session) {
-    supabase.auth.setSession(session);
-    return NextResponse.json({
-      message: "Sign In successful!",
-      access_token: session.access_token,
-      refresh_token: session.refresh_token,
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
     });
-  }
 
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    const { session } = data;
+
+    if (session) {
+      supabase.auth.setSession(session);
+      return NextResponse.json({
+        message: "Sign In successful!",
+        access_token: session.access_token,
+        refresh_token: session.refresh_token,
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
   return NextResponse.json({ error: "No Session" }, { status: 401 });
 }
