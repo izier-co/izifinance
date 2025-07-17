4d3454c1ec58eedd5e40230db57fe87882f1c892
testboiz <tvmini0202@gmail.com>
2025-07-17 12:15:11 +0700

    refactor (dashboard) : move sidebar and breadcrumb to layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,39 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 w-full">
+        <SidebarTrigger className="-ml-1" />
+        <Separator
+          orientation="vertical"
+          className="mr-2 data-[orientation=vertical]:h-4"
+        />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+      <main>
+        <SidebarInset>{children}</SidebarInset>
+      </main>
+    </SidebarProvider>
+  );
+}
