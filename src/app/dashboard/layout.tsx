a7b7bf016bf03ddbc2801dddb178d788bebd58a9
testboiz <tvmini0202@gmail.com>
2025-07-17 16:35:26 +0700

    style (dashboard) : add placeholder content

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,17 +12,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 w-full">
-        <SidebarTrigger className="-ml-1" />
-        <Separator
-          orientation="vertical"
-          className="mr-2 data-[orientation=vertical]:h-4"
-        />
-        <DynamicBreadcrumbs />
-      </header>
-      <main>
-        <SidebarInset>{children}</SidebarInset>
-      </main>
+      <div className="flex flex-col w-full">
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 w-full">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <DynamicBreadcrumbs />
+        </header>
+        <main>
+          <SidebarInset>{children}</SidebarInset>
+        </main>
+      </div>
     </SidebarProvider>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,43 @@
+import { ChartLineDefault } from "@/components/chart-line-default";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Page() {
   return (
     // {/* Suspense Elements */}
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <Card>
+          <CardHeader>
+            <CardTitle>Reimbursements</CardTitle>
+          </CardHeader>
+          <CardContent>+3 more notes since last 24 hours</CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Notes</CardTitle>
+          </CardHeader>
+          <CardContent>
+            IDR 240.350 worth of items pending approval overall
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Orders</CardTitle>
+          </CardHeader>
+          <CardContent>
+            IDR 150.000 worth of subscription fees are due this week
+          </CardContent>
+        </Card>
       </div>
-      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+      <ChartLineDefault />
     </div>
   );
 }
