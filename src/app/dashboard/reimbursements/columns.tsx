da94d538f8cb9219a21b913aee0d3e51eab3a81c
testboiz <tvmini0202@gmail.com>
2025-07-21 16:29:32 +0700

    refactor (datatable) : split remaining table header to the new ui component

--- src/app/dashboard/reimbursements/columns.tsx
+++ src/app/dashboard/reimbursements/columns.tsx
@@ -106,15 +106,7 @@ export const columns: ColumnDef<Reimbursements>[] = [
   {
     accessorKey: "inCategoryID",
     header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Category
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
+      return <SortableHeader column={column} title="Category" />;
     },
   },
   {
