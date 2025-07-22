7a46ed4c3e392bb30d1fd9864cf7851c3695f356
testboiz <tvmini0202@gmail.com>
2025-07-22 10:45:05 +0700

    feat : add display of parent reimbursement note data

--- src/app/(logged-in)/dashboard/reimbursements/[id]/page.tsx
+++ src/app/(logged-in)/dashboard/reimbursements/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { fetchJSONAPI } from "@/lib/server-lib";
-import { columns, ReimbursementItems } from "./columns";
+import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 
-async function getData(id: string): Promise<Array<ReimbursementItems>> {
+async function getData(id: string) {
   const data = await fetchJSONAPI("GET", `/api/v1/reimbursements/${id}/notes`);
   const json = await data.json();
-  return json["data"][0]["reimbursement_items"];
+  return json["data"][0];
 }
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
@@ -13,7 +13,23 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   const data = await getData(params.id);
   return (
     <div className="">
-      <DataTable columns={columns} data={data} />
+      <ol>
+        <li>Created At : {data["daCreatedAt"]}</li>
+        <li>Updated At : {data["daUpdatedAt"]}</li>
+        <li>Reimbursement ID : {data["txReimbursementNoteID"]}</li>
+        <li>Status : {data["txStatus"]}</li>
+        <li>Description : {data["txDescriptionDetails"]}</li>
+        <li>Recipient Account : {data["txRecipientAccount"]}</li>
+        <li>Bank Type Code : {data["inBankTypeCode"]}</li>
+        <li>Recipient Company Code : {data["inRecipientCompanyCode"]}</li>
+        <li>Bank Account Code : {data["txBankAccountCode"]}</li>
+        <li>Change Reason : {data["txChangeReason"]}</li>
+        <li>Employee Code : {data["txEmployeeCode"]}</li>
+        <li>Approved By : {data["txApprovedBy"]}</li>
+        <li>Total Reimbursement : {data["dcNominalReimbursement"]}</li>
+        <li>Category ID : {data["inCategoryID"]}</li>
+      </ol>
+      <DataTable columns={columns} data={data["reimbursement_items"]} />
     </div>
   );
 }
