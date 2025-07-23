a8c7b05c49a35d0cacde0cf03887491ee845e28e
testboiz <tvmini0202@gmail.com>
2025-07-23 08:35:27 +0700

    todo (lib) : fix server and client mismatch issue

--- src/lib/server-lib.ts
+++ src/lib/server-lib.ts
@@ -7,8 +7,16 @@ export async function fetchJSONAPI(
   jsonBody?: object
 ) {
   const cookieHeaders = await headers();
-  const baseUrl = "http://localhost:3000";
-  return await fetch(baseUrl + url, {
+
+  const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+  const host = cookieHeaders.get("host");
+
+  if (!host) throw new Error("Cannot determine URL! Missing host header");
+
+  const domain = `${protocol}://${host}`;
+  const fullURL = domain + url;
+
+  return await fetch(fullURL, {
     method: method,
     headers: {
       "Content-Type": "application/json",
