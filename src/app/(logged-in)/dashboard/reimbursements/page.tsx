d725a3476f67971b7a9a9aba51a0c29367651fab
testboiz <tvmini0202@gmail.com>
2025-07-25 09:40:51 +0700

    feat (reimbursements) : add button to add reimbursements

--- src/app/(logged-in)/dashboard/reimbursements/page.tsx
+++ src/app/(logged-in)/dashboard/reimbursements/page.tsx
@@ -1,6 +1,8 @@
 import { fetchJSONAPI } from "@/lib/lib";
 import { columns, Reimbursements } from "./columns";
 import { DataTable } from "@/components/data-table";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 async function getData(): Promise<Array<Reimbursements>> {
   const data = await fetchJSONAPI("GET", "/api/v1/reimbursements");
@@ -11,7 +13,10 @@ async function getData(): Promise<Array<Reimbursements>> {
 export default async function Page() {
   const data = await getData();
   return (
-    <div className="">
+    <div className="flex flex-col">
+      <Link className="ml-auto mb-2" href="/dashboard/reimbursements/add">
+        <Button className="">Add Reimbursement</Button>
+      </Link>
       <DataTable columns={columns} data={data} />
     </div>
   );
