61f079786edd06602e33c6e5e2047f7952dc2594
testboiz <tvmini0202@gmail.com>
2025-07-29 08:39:02 +0700

    style (reimbursement item) : use shadcn table and add margins

--- src/app/(logged-in)/dashboard/reimbursements/[id]/page.tsx
+++ src/app/(logged-in)/dashboard/reimbursements/[id]/page.tsx
@@ -2,6 +2,16 @@ import { fetchJSONAPI } from "@/lib/lib";
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 async function getData(id: string) {
   const data = await fetchJSONAPI("GET", `/api/v1/reimbursements/${id}/notes`);
   const json = await data.json();
@@ -13,23 +23,75 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   const data = await getData(params.id);
   return (
     <div className="">
-      <ol>
-        <li>Created At : {data["daCreatedAt"]}</li>
-        <li>Updated At : {data["daUpdatedAt"]}</li>
-        <li>Reimbursement ID : {data["txReimbursementNoteID"]}</li>
-        <li>Status : {data["txStatus"]}</li>
-        <li>Description : {data["txDescriptionDetails"]}</li>
-        <li>Recipient Account : {data["txRecipientAccount"]}</li>
-        <li>Bank Type Code : {data["inBankTypeCode"]}</li>
-        <li>Recipient Company Code : {data["inRecipientCompanyCode"]}</li>
-        <li>Bank Account Code : {data["txBankAccountCode"]}</li>
-        <li>Change Reason : {data["txChangeReason"]}</li>
-        <li>Employee Code : {data["txEmployeeCode"]}</li>
-        <li>Approved By : {data["txApprovedBy"]}</li>
-        <li>Total Reimbursement : {data["dcNominalReimbursement"]}</li>
-        <li>Category ID : {data["inCategoryID"]}</li>
-      </ol>
-      <DataTable columns={columns} data={data["reimbursement_items"]} />
+      <Table className="mb-6 max-w-[80%] mx-auto">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Information</TableHead>
+            <TableHead>Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>Created At</TableCell>
+            <TableCell>{data["daCreatedAt"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Updated At</TableCell>
+            <TableCell>{data["daUpdatedAt"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Reimbursement ID</TableCell>
+            <TableCell>{data["txReimbursementNoteID"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Status</TableCell>
+            <TableCell>{data["txStatus"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Description</TableCell>
+            <TableCell>{data["txDescriptionDetails"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Recipient Account</TableCell>
+            <TableCell>{data["txRecipientAccount"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Bank Type Code</TableCell>
+            <TableCell>{data["inBankTypeCode"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Recipient Company Code</TableCell>
+            <TableCell>{data["inRecipientCompanyCode"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Bank Account Code</TableCell>
+            <TableCell>{data["txBankAccountCode"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Change Reason</TableCell>
+            <TableCell>{data["txChangeReason"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Employee Code</TableCell>
+            <TableCell>{data["txEmployeeCode"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Approved By</TableCell>
+            <TableCell>{data["txApprovedBy"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Total Reimbursement</TableCell>
+            <TableCell>{data["dcNominalReimbursement"]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Category ID</TableCell>
+            <TableCell>{data["inCategoryID"]}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <div className="mx-[10%]">
+        <DataTable columns={columns} data={data["reimbursement_items"]} />
+      </div>
     </div>
   );
 }
