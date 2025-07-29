a7852a373ba6e43c9b3501bb4c5cb6d50e960ad7
testboiz <tvmini0202@gmail.com>
2025-07-29 11:51:54 +0700

    style (dashbiard) : add dynamic informative message

--- src/app/(logged-in)/dashboard/page.tsx
+++ src/app/(logged-in)/dashboard/page.tsx
@@ -1,30 +1,71 @@
 import { ChartLineDefault } from "@/components/chart-line-default";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { fetchJSONAPI } from "@/lib/lib";
 
-export default function Page() {
+async function getDashboardData() {
+  const url = "/api/v1/reimbursements?";
+  const MILISECONDS_IN_DAY = 1000 * 60 * 60 * 24;
+  const NOW = Date.now();
+  const oneDayParams = new URLSearchParams({
+    createdAfter: new Date(NOW - MILISECONDS_IN_DAY).toISOString(),
+  }).toString();
+  const pendingParams = new URLSearchParams({
+    status: "Pending",
+  }).toString();
+  const idrPendingParams = new URLSearchParams({
+    currency: "IDR",
+    status: "Pending",
+  }).toString();
+  const oneDayReimbursements = await fetchJSONAPI("GET", url + oneDayParams);
+  const oneDayJson = await oneDayReimbursements.json();
+  const reimbursementInOneDayCount = oneDayJson.data.length;
+  const pendingReimbursements = await fetchJSONAPI("GET", url + pendingParams);
+  const pendingJson = await pendingReimbursements.json();
+  const pendingReimbursementsCount = pendingJson.data.length;
+  const idrPendingReimbursements = await fetchJSONAPI(
+    "GET",
+    url + idrPendingParams
+  );
+  const idrPendingJson = await idrPendingReimbursements.json();
+  let totalPending = 0;
+  for (let i = 0; i < idrPendingJson.data.length; i++) {
+    totalPending = idrPendingJson.data[i].deTotalReimbursement;
+  }
+  return {
+    oneDayCount: reimbursementInOneDayCount,
+    pendingCount: pendingReimbursementsCount,
+    pendingValue: totalPending,
+  };
+}
+
+export default async function Page() {
+  const displayData = await getDashboardData();
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <Card>
           <CardHeader>
-            <CardTitle>Reimbursements</CardTitle>
-          </CardHeader>
-          <CardContent>+3 more notes since last 24 hours</CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Notes</CardTitle>
+            <CardTitle>New Reimbursements</CardTitle>
           </CardHeader>
           <CardContent>
-            IDR 240.350 worth of items pending approval overall
+            +{displayData.oneDayCount} more notes since last 24 hours
           </CardContent>
         </Card>
         <Card>
           <CardHeader>
-            <CardTitle>Orders</CardTitle>
+            <CardTitle>Pending Approval Notes</CardTitle>
           </CardHeader>
           <CardContent>
-            IDR 150.000 worth of subscription fees are due this week
+            {displayData.pendingCount} notes are pending approval overall
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Reimbursement Value</CardTitle>
+          </CardHeader>
+          <CardContent>
+            IDR {displayData.pendingValue} worth of reimbursements are still
+            pending
           </CardContent>
         </Card>
       </div>
