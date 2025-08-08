bf2728ef9994cb2d4f3bff82587a1950c1258a5f
testboiz <tvmini0202@gmail.com>
2025-08-08 16:09:43 +0700

    feat (datatable) : migrated datatable to server datatable

--- src/app/(logged-in)/dashboard/categories/page.tsx
+++ src/app/(logged-in)/dashboard/categories/page.tsx
@@ -1,31 +1,11 @@
-"use client";
-import { fetchJSONAPI } from "@/lib/lib";
 import { columns } from "./columns";
-import { DataTable } from "@/components/data-table";
-import { useQuery } from "@tanstack/react-query";
-
-async function getData() {
-  const data = await fetchJSONAPI("GET", "/api/v1/categories");
-  const json = await data.json();
-  return json.data;
-}
+import { ServerDataTable } from "@/components/server-datatable";
 
 export default function Page() {
-  const dataQuery = useQuery({
-    queryKey: ["reimbursement-query"],
-    queryFn: getData,
-  });
-  if (dataQuery.isLoading) {
-    return <>Loading</>;
-  }
-  if (dataQuery.error) {
-    console.error(dataQuery.error.message);
-    return <>Error : {dataQuery.error.message}</>;
-  }
   return (
     <>
       <h1 className="text-bold mb-4">Categories</h1>
-      <DataTable columns={columns} data={dataQuery.data} />
+      <ServerDataTable columns={columns} rowName="categories" />
     </>
   );
 }
--- src/app/(logged-in)/dashboard/reimbursements/page.tsx
+++ src/app/(logged-in)/dashboard/reimbursements/page.tsx
@@ -1,32 +1,11 @@
-"use client";
-
-import { fetchJSONAPI } from "@/lib/lib";
 import { columns } from "./columns";
-import { DataTable } from "@/components/data-table";
-import { useQuery } from "@tanstack/react-query";
-
-async function getData() {
-  const data = await fetchJSONAPI("GET", "/api/v1/reimbursements");
-  const json = await data.json();
-  return json.data;
-}
+import { ReimbursementDatatable } from "@/components/reimbursement-datatable";
 
 export default function Page() {
-  const dataQuery = useQuery({
-    queryKey: ["reimbursement-query"],
-    queryFn: getData,
-  });
-  if (dataQuery.isLoading) {
-    return <>Loading</>;
-  }
-  if (dataQuery.error) {
-    console.error(dataQuery.error.message);
-    return <>Error : {dataQuery.error.message}</>;
-  }
   return (
     <>
       <h1 className="text-bold mb-4">Reimbursements</h1>
-      <DataTable columns={columns} data={dataQuery.data} />
+      <ReimbursementDatatable columns={columns} />
     </>
   );
 }
