db9f461a7e06906941dfbc6f4ab3483379f47caf
testboiz <tvmini0202@gmail.com>
2025-08-09 08:16:12 +0700

    fix : delete unused schema

--- schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-
-export const reimbursementNotesTable = pgTable('users', {
-  id: uuid(),
-  fullName: text('full_name'),
-  phone: varchar('phone', { length: 256 }),
-});
