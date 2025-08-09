8015ee966e32285ef88081b9f1413bf100755778
testboiz <tvmini0202@gmail.com>
2025-08-09 13:08:02 +0700

    refactor (query) : refactor get employee id code

---
+++ src/queries/queries.tsx
@@ -0,0 +1,31 @@
+import { supabase } from "@/app/api/supabase.config";
+import { fetchJSONAPI } from "@/lib/lib";
+import { useQuery } from "@tanstack/react-query";
+
+async function getEmpID() {
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  if (data.user === null) {
+    throw new Error("Unauthorized User");
+  }
+
+  const res = await fetchJSONAPI("GET", `/api/v1/employees/${data.user.id}`);
+  const json = await res.json();
+  if (!res.ok) {
+    throw new Error(json.error);
+  }
+  if (json.data.length === 0) {
+    throw new Error("Unauthorized User");
+  }
+  return json.data[0].txEmployeeCode;
+}
+
+export function useEmployeeIDQuery() {
+  return useQuery({
+    queryKey: ["emp-id"],
+    queryFn: getEmpID,
+  });
+}
