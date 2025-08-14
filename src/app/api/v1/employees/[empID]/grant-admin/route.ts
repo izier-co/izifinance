fb497c3f1d35d34f85ed20439174eee98f373546
testboiz <tvmini0202@gmail.com>
2025-08-14 12:44:05 +0700

    feat (API) : add grant and revoke admin access endpoints

---
+++ src/app/api/v1/employees/[empID]/grant-admin/route.ts
@@ -0,0 +1,33 @@
+import { createClient } from "@/app/api/supabase_server.config";
+import { authorizeAdmin, removeByKey } from "@/lib/lib";
+import { NextRequest, NextResponse } from "next/server";
+
+export const PUT = async (
+  req: NextRequest,
+  props: { params: Promise<{ empID: string }> }
+) => {
+  const supabase = await createClient();
+  const unauthorizedResponse = await authorizeAdmin(supabase);
+  if (unauthorizedResponse) return unauthorizedResponse;
+
+  const params = await props.params;
+  const id = params.empID;
+
+  const { data, error } = await supabase
+    .from("m_employees")
+    .update({
+      boHasAdminAccess: true,
+    })
+    .eq("txReimbursementNoteID", id)
+    .select();
+
+  if (error)
+    return NextResponse.json({ error: error.message }, { status: 500 });
+
+  const sanitizedData = removeByKey(data);
+
+  return NextResponse.json({
+    message: "Note Approved!",
+    data: sanitizedData,
+  });
+};
