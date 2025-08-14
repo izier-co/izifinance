b597ef2566d78bd48d1787941cd692bb13c2da92
testboiz <tvmini0202@gmail.com>
2025-08-14 14:16:57 +0700

    feat (dropdown menu) : add grant and revoke admin functionality

--- src/app/(logged-in)/dashboard/employees/_components/dropdown_menu.tsx
+++ src/app/(logged-in)/dashboard/employees/_components/dropdown_menu.tsx
@@ -16,12 +16,33 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
+import { fetchJSONAPI } from "@/lib/lib";
+import { refreshAndRevalidatePage } from "@/lib/server-lib";
 import { useEmployeeIDQuery } from "@/queries/queries";
+import { useMutation } from "@tanstack/react-query";
 import { Row } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
+import { Loader2, MoreHorizontal } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 function GrantAdminDialog({ row }: { row: Row<CommonRow> }) {
+  const [error, setError] = useState("");
+  const grantAdminQuery = useMutation({
+    mutationKey: ["grant-admin"],
+    mutationFn: async () => {
+      await fetchJSONAPI(
+        "PUT",
+        `/api/v1/employees/${row.getValue("txEmployeeCode")}/grant-admin`
+      );
+    },
+    onError: (error) => {
+      console.error(error);
+      setError(error.message);
+    },
+    onMutate: () => {
+      refreshAndRevalidatePage("/dashboard/employees");
+    },
+  });
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -38,13 +59,27 @@ function GrantAdminDialog({ row }: { row: Row<CommonRow> }) {
         <DialogDescription>
           Are you sure to grant admin access to {row.getValue("txFullName")}
         </DialogDescription>
+        {error && (
+          <p className="text-sm font-medium text-destructive mb-2">{error}</p>
+        )}
         <DialogFooter>
           <DialogClose asChild>
             <Button variant="secondary" type="button">
               Cancel
             </Button>
           </DialogClose>
-          <Button type="button">Grant</Button>
+          <Button
+            type="button"
+            onClick={() => {
+              grantAdminQuery.mutate();
+            }}
+          >
+            {grantAdminQuery.isPending ? (
+              <Loader2 className="w-4 h-4 animate-spin" />
+            ) : (
+              "Grant"
+            )}
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
@@ -52,6 +87,24 @@ function GrantAdminDialog({ row }: { row: Row<CommonRow> }) {
 }
 
 function RevokeAdminDialog({ row }: { row: Row<CommonRow> }) {
+  const [error, setError] = useState("");
+  const revokeAdminQuery = useMutation({
+    mutationKey: ["grant-admin"],
+    mutationFn: async () => {
+      const res = await fetchJSONAPI(
+        "PUT",
+        `/api/v1/employees/${row.getValue("txEmployeeCode")}/revoke-admin`
+      );
+      console.log(await res.json());
+    },
+    onError: (error) => {
+      console.error(error);
+      setError(error.message);
+    },
+    onMutate: () => {
+      refreshAndRevalidatePage("/dashboard/employees");
+    },
+  });
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -68,13 +121,27 @@ function RevokeAdminDialog({ row }: { row: Row<CommonRow> }) {
         <DialogDescription>
           Are you sure to revoke admin access to {row.getValue("txFullName")}
         </DialogDescription>
+        {error && (
+          <p className="text-sm font-medium text-destructive mb-2">{error}</p>
+        )}
         <DialogFooter>
           <DialogClose asChild>
             <Button variant="secondary" type="button">
               Cancel
             </Button>
           </DialogClose>
-          <Button type="button">Revoke</Button>
+          <Button
+            type="button"
+            onClick={() => {
+              revokeAdminQuery.mutate();
+            }}
+          >
+            {revokeAdminQuery.isPending ? (
+              <Loader2 className="w-4 h-4 animate-spin" />
+            ) : (
+              "Revoke"
+            )}
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
@@ -86,6 +153,9 @@ function GrantRevokeDialogMenu({ row }: { row: Row<CommonRow> }) {
   const isAdmin = checkAdminQuery.isSuccess && checkAdminQuery.data;
   if (!isAdmin) {
     return;
+  }
+  if (checkAdminQuery.data === row.getValue("txEmployeeCode")) {
+    return; // to avoid bugs after setting self not being admin
   }
   const adminStatus = row.getValue("boHasAdminAccess") as boolean;
   if (adminStatus === true) {
