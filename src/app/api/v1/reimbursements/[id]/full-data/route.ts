01e8c1225e37c35b2ac5b1a319bfa182a2195ad5
testboiz <tvmini0202@gmail.com>
2025-08-16 14:18:22 +0700

    feat (reimbursement) : add complete view of reimbursements

---
+++ src/app/api/v1/reimbursements/[id]/full-data/route.ts
@@ -0,0 +1,30 @@
+import { removeByKey } from "@/lib/lib";
+import { createClient } from "@/app/api/supabase_server.config";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  req: NextRequest,
+  props: { params: Promise<{ id: string }> }
+) => {
+  const supabase = await createClient();
+  const urlParams = await props.params;
+
+  const { data, error } = await supabase
+    .from("reimbursement_notes")
+    .select(
+      "*, reimbursement_items(*), m_category(*), issuer_emp_data:txEmployeeCode(*), admin_emp_data:txChangedBy(*)"
+    )
+    .eq("txReimbursementNoteID", urlParams.id);
+
+  if (error)
+    return NextResponse.json({ error: error.message }, { status: 500 });
+
+  const sanitizedData = removeByKey(data);
+  return NextResponse.json(
+    {
+      data: sanitizedData,
+      pagination: {},
+    },
+    { status: 200 }
+  );
+};
