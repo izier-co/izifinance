aff847983e1332e2c50b241fb159f8940351da63
testboiz <tvmini0202@gmail.com>
2025-08-16 12:50:30 +0700

    feat (ui) : add dialogs

--- src/app/(logged-in)/dashboard/settings/page.tsx
+++ src/app/(logged-in)/dashboard/settings/page.tsx
@@ -1,4 +1,14 @@
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Page() {
   return (
@@ -11,27 +21,87 @@ export default function Page() {
             <h2>Profile Picture</h2>
             <p className="text-sm">Update Profile Picture</p>
           </div>
-          <Button className="ml-auto">Upload</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="ml-auto">Upload</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Confirmation</DialogTitle>
+                <DialogDescription>
+                  Upload your new profile picture
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="secondary" type="button">
+                    Cancel
+                  </Button>
+                </DialogClose>
+                <Button type="button">Confirm</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
         <hr className="my-2" />
         <div className="flex">
           <div>
             <h2>Change Email</h2>
             <p className="text-sm text-destructive">
-              Warning : This change would log you out
+              Danger Zone : This change would log you out
             </p>
           </div>
-          <Button className="ml-auto">Change Email</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="ml-auto">Change Email</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Input new Email</DialogTitle>
+                <DialogDescription>
+                  This action can&apos;t be undone, you would be logged off
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="secondary" type="button">
+                    Cancel
+                  </Button>
+                </DialogClose>
+                <Button type="button">Confirm</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
         <hr className="my-2" />
         <div className="flex">
           <div>
             <h2>Change Password</h2>
             <p className="text-sm text-destructive">
-              Warning : This change would log you out
+              Danger Zone : This change would log you out
             </p>
           </div>
-          <Button className="ml-auto">Change Password</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="ml-auto">Change Password</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Input new Password</DialogTitle>
+                <DialogDescription>
+                  This action can&apos;t be undone, you would be logged off
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="secondary" type="button">
+                    Cancel
+                  </Button>
+                </DialogClose>
+                <Button type="button">Confirm</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </>
