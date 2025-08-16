bd0f50343c61a60d5d3adab6d457a8d2a0806bac
testboiz <tvmini0202@gmail.com>
2025-08-16 15:08:47 +0700

    feat (ui) : add basic datatable ui

---
+++ src/app/(logged-in)/dashboard/admin/users/columns.tsx
@@ -0,0 +1,68 @@
+import {
+  CommonRow,
+  SortableHeader,
+} from "@/components/sorting-datatable-header";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ColumnDef } from "@tanstack/react-table";
+import { User } from "lucide-react";
+import { UserDropdownMenu } from "./_components/dropdown_menu";
+
+export const columns: ColumnDef<CommonRow>[] = [
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+      return <SortableHeader column={column} title="Created At" />;
+    },
+    cell: ({ row }) => {
+      const dateFromISO = new Date(row.getValue("daCreatedAt"));
+      const localTime = dateFromISO.toLocaleString();
+      return <div>{localTime}</div>;
+    },
+  },
+  {
+    accessorKey: "updatedAt",
+    header: ({ column }) => {
+      return <SortableHeader column={column} title="Updated At" />;
+    },
+    cell: ({ row }) => {
+      const dateFromISO = new Date(row.getValue("daUpdatedAt"));
+      const localTime = dateFromISO.toLocaleString();
+      return <div>{localTime}</div>;
+    },
+  },
+  {
+    accessorKey: "id",
+    header: ({ column }) => {
+      return <SortableHeader column={column} title="User ID" />;
+    },
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return <SortableHeader column={column} title="Email" />;
+    },
+  },
+  {
+    accessorKey: "profile_picture",
+    header: ({ column }) => {
+      return <SortableHeader column={column} title="Profile Picture" />;
+    },
+    cell: ({ row }) => {
+      const url = row.getValue("profile_picture") as string;
+      return (
+        <Avatar className="h-8 w-8 rounded-lg">
+          <AvatarImage src={url} alt="Avatar Image" />
+          <AvatarFallback className="rounded-lg">
+            <User />
+          </AvatarFallback>
+        </Avatar>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: () => {
+      return <UserDropdownMenu />;
+    },
+  },
+];
--- src/app/(logged-in)/dashboard/admin/users/page.tsx
+++ src/app/(logged-in)/dashboard/admin/users/page.tsx
@@ -1,3 +1,11 @@
+import { DataTable } from "@/components/data-table";
+import { columns } from "./columns";
+
 export default function Page() {
-  return <> Users</>;
+  return (
+    <>
+      <h1>Users Page</h1>
+      <DataTable columns={columns} data={{}} />
+    </>
+  );
 }
