9b418ba1d1ede8effe24d9d1f02954314386a539
testboiz <tvmini0202@gmail.com>
2025-08-17 12:26:45 +0700

    feat (user management) : add responses  and refresh session

--- src/app/api/v1/auth/update-credentials/route.ts
+++ src/app/api/v1/auth/update-credentials/route.ts
@@ -49,4 +49,10 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
   }
+
+  const { error } = await supabase.auth.refreshSession();
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+  return NextResponse.json({ message: "Successfully changed credentials" });
 }
