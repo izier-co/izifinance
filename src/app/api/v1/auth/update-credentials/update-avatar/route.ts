1c0b3c773167934e4581265d2dbb73d1c0682a14
testboiz <tvmini0202@gmail.com>
2025-08-18 21:59:44 +0700

    feat (API) : implement upload profile picture

---
+++ src/app/api/v1/auth/update-credentials/update-avatar/route.ts
@@ -0,0 +1,57 @@
+import { createClient } from "@/app/api/supabase_server.config";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const supabase = await createClient();
+  try {
+    const formData = await req.formData();
+    const file = formData.get("image") as File | null;
+
+    if (!file) {
+      return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+    }
+
+    const { data: userData, error: userError } = await supabase.auth.getUser();
+    if (userError) {
+      return NextResponse.json({ error: userError.message }, { status: 500 });
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const filename = `profile-pictures/avatar-${userData.user.id}`;
+
+    const { data, error } = await supabase.storage
+      .from("profile-pictures")
+      .upload(filename, buffer, {
+        contentType: file.type,
+        upsert: true,
+      });
+
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    const {
+      data: { publicUrl },
+    } = supabase.storage.from("profile-pictures").getPublicUrl(filename);
+
+    const { error: metadataError } = await supabase.auth.updateUser({
+      data: {
+        profile_picture: publicUrl,
+      },
+    });
+
+    if (metadataError) {
+      return NextResponse.json(
+        { error: metadataError.message },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json({ path: data.path }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json(
+      { error: (err as Error).message },
+      { status: 500 }
+    );
+  }
+}
