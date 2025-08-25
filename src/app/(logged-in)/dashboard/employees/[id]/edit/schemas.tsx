7b09e66e01f1075df5b354397b2a2ce27b974c24
testboiz <tvmini0202@gmail.com>
2025-08-25 16:03:59 +0700

    feat (ui) : add edit employee ui

---
+++ src/app/(logged-in)/dashboard/employees/[id]/edit/schemas.tsx
@@ -0,0 +1,38 @@
+import constValues from "@/lib/constants";
+import { isValidInt } from "@/lib/lib";
+import z from "zod";
+
+export const editEmployeeSchema = z.object({
+  txFullName: z
+    .string()
+    .nonempty("Input can't be empty")
+    .max(constValues.maxShortTextLength),
+  daDateOfBirth: z.iso.date(),
+  txHomeAddress: z
+    .string()
+    .nonempty("Input can't be empty")
+    .max(constValues.maxTextLength),
+  txReligionCode: z.string().nonempty("Input can't be empty"),
+  txTaxNumber: z
+    .string()
+    .nonempty("Input can't be empty")
+    .refine((num) => isValidInt(num), "Must be numerical string"),
+  boMarriageStatus: z.boolean(),
+  inNumOfDeps: z.number().int("Must be an integer"),
+  flSalary: z.float32("Must be a number"),
+  txRoleCode: z.string(),
+  txEmploymentTypeCode: z.string(),
+  txCompanyCode: z.string(),
+  txPhoneNumber: z
+    .string()
+    .nonempty("Input can't be empty")
+    .refine((num) => isValidInt(num), "Must be numerical string"),
+  txEmailAddress: z.email("Must be valid email"),
+  txBankTypeCode: z.string(),
+  txBankAccountNumber: z
+    .string()
+    .nonempty("Input can't be empty")
+    .refine((num) => isValidInt(num), "Must be numerical string"),
+});
+
+export type EditEmployeeSchema = z.infer<typeof editEmployeeSchema>;
