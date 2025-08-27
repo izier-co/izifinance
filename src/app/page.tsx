a7a8bcfc225eff0b63d4cf523198203ec4cd21e7
testboiz <tvmini0202@gmail.com>
2025-08-27 09:17:58 +0700

    fix (merge) : fix mere issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,14 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Loader2, Lock, Mail } from "lucide-react";
 
 import { useForm } from "react-hook-form";
@@ -17,6 +24,7 @@ import { supabase } from "./api/supabase.config";
 import { emailSchema, passwordSchema } from "@/schemas/schema";
 import Image from "next/image";
 import Link from "next/link";
+import z from "zod";
 
 const loginSchema = z.object({
   email: emailSchema,
@@ -56,7 +64,10 @@ export default function Home() {
     if (data.user === null) {
       throw new Error("Unregistered Account");
     }
-    const empRes = await fetchJSONAPI("GET", `/api/v1/employees/get-id/${data.user.id}`);
+    const empRes = await fetchJSONAPI(
+      "GET",
+      `/api/v1/employees/get-id/${data.user.id}`
+    );
     const json = await empRes.json();
 
     if (!empRes.ok) {
@@ -92,17 +103,31 @@ export default function Home() {
         <div className={cn("flex flex-col gap-6")}>
           <div className="flex flex-col xl:flex-row items-center gap-16">
             <div className="flex flex-col items-center justify-center md:justify-end w-full md:w-1/3">
-              <Image src="/images/logo.jpg" alt="Logo" width={150} height={150} className="xl:w-200  object-contain" />
-              <h1 className="font-bold text-2xl xl:text-3xl mt-2">Izifinance</h1>
+              <Image
+                src="/images/logo.jpg"
+                alt="Logo"
+                width={150}
+                height={150}
+                className="xl:w-200  object-contain"
+              />
+              <h1 className="font-bold text-2xl xl:text-3xl mt-2">
+                Izifinance
+              </h1>
             </div>
             <div className="w-full xl:w-2/3">
               <Card>
                 <CardHeader>
-                  <CardTitle className="flex items-center justify-center">Login to your account</CardTitle>
+                  <CardTitle className="flex items-center justify-center">
+                    Login to your account
+                  </CardTitle>
                 </CardHeader>
                 <CardContent>
                   <Form {...form}>
-                    <form id="login-form" className="flex flex-col gap-4 " onSubmit={form.handleSubmit(onSubmit)}>
+                    <form
+                      id="login-form"
+                      className="flex flex-col gap-4 "
+                      onSubmit={form.handleSubmit(onSubmit)}
+                    >
                       <div className="flex flex-row items-center gap-2">
                         <Mail className="text-[var(--sidebar-accent-foreground)]" />
                         <FormField
@@ -110,9 +135,14 @@ export default function Home() {
                           name="email"
                           render={({ field }) => (
                             <FormItem className="w-full">
-                              <FormLabel className="capitalize not-italic">Email</FormLabel>
+                              <FormLabel className="capitalize not-italic">
+                                Email
+                              </FormLabel>
                               <FormControl>
-                                <Input placeholder="john.doe@example.com" {...field} />
+                                <Input
+                                  placeholder="john.doe@example.com"
+                                  {...field}
+                                />
                               </FormControl>
                               <FormMessage />
                             </FormItem>
@@ -126,10 +156,16 @@ export default function Home() {
                           name="password"
                           render={({ field }) => (
                             <FormItem className="w-full">
-                              <FormLabel className="capitalize not-italic">Password</FormLabel>
+                              <FormLabel className="capitalize not-italic">
+                                Password
+                              </FormLabel>
                               <div className="flex flex-row gap-1 justify-center item-center">
                                 <FormControl>
-                                  <Input type={showPassword ? "text" : "password"} placeholder="Type your password here" {...field} />
+                                  <Input
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder="Type your password here"
+                                    {...field}
+                                  />
                                 </FormControl>
                               </div>
                               <FormMessage />
@@ -137,16 +173,32 @@ export default function Home() {
                           )}
                         />
                       </div>
-                      <Button form="login-form" type="submit" disabled={loading} className="bg-[var(--primarybtn)] text-white hover:bg-[var(--primarybtnhover)]">
-                        {loading ? <Loader2 className="w-4 h-4 animate-spin " /> : "Login"}
+                      <Button
+                        form="login-form"
+                        type="submit"
+                        disabled={loading}
+                        className="bg-[var(--primarybtn)] text-white hover:bg-[var(--primarybtnhover)]"
+                      >
+                        {loading ? (
+                          <Loader2 className="w-4 h-4 animate-spin " />
+                        ) : (
+                          "Login"
+                        )}
                       </Button>
 
-                      {form.formState.errors.root?.message && <p className="text-sm font-medium text-destructive">{form.formState.errors.root.message}</p>}
+                      {form.formState.errors.root?.message && (
+                        <p className="text-sm font-medium text-destructive">
+                          {form.formState.errors.root.message}
+                        </p>
+                      )}
                     </form>
                   </Form>
                 </CardContent>
               </Card>
-              <Link href="forgotPassword.tsx" className="flex justify-end font-light text-[var(--sidebar-accent-foreground)] hover:underline">
+              <Link
+                href="forgotPassword.tsx"
+                className="flex justify-end font-light text-[var(--sidebar-accent-foreground)] hover:underline"
+              >
                 Forgot Password?
               </Link>
             </div>
