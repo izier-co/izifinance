d4d3ef2ca5459982af70f2faf2b56a3db2a4fad7
elisahardian <elisahardian005@gmail.com>
2025-09-06 11:51:45 +0700

    feat: improve error page

--- src/app/(logged-in)/dashboard/reimbursements/[id]/error.tsx
+++ src/app/(logged-in)/dashboard/reimbursements/[id]/error.tsx
@@ -1,5 +1,23 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { TriangleAlert } from "lucide-react";
+import Link from "next/link";
+
 export default function Page() {
-  return <>Something Went Wrong</>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-160 content-center">
+        <div className="mb-4 ">
+          <TriangleAlert className="size-30 mb-8 text-[var(--destructive)]" />
+          <p className="font-bold mb-2">Opss..</p>
+          <p className="font-bold text-3xl mb-4">Something Went Wrong</p>
+          <p className="mb-4">We are working on fixing the problem. Please try again later.</p>
+        </div>
+        <Link href="/dashboard">
+          <Button variant="default">Go Back to Dashboard</Button>
+        </Link>
+      </div>
+    </>
+  );
 }
