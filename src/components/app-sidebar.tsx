6e43ada198c0edb00641a6992284e470f0cf01c9
testboiz <tvmini0202@gmail.com>
2025-09-08 12:39:37 +0700

    fix (sidebar) : add parameter for admin access

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,8 +1,37 @@
 import * as React from "react";
-import { FilePlus, FolderCog, IdCardLanyard, ListPlus, Minus, Plus, ReceiptText, Tag, UserPen, UserStar, Wallet } from "lucide-react";
+import {
+  FilePlus,
+  FolderCog,
+  IdCardLanyard,
+  ListPlus,
+  Minus,
+  Plus,
+  ReceiptText,
+  Tag,
+  UserPen,
+  UserStar,
+  Wallet,
+} from "lucide-react";
 
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem, SidebarRail } from "@/components/ui/sidebar";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
 import Link from "next/link";
 import Image from "next/image";
 import { getEmpInfo, getUser } from "@/queries/server-queries";
@@ -98,13 +127,16 @@ const userSidebarData = {
   ],
 };
 
-export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export async function AppSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
   const empData = await getEmpInfo();
   const supabaseUser = await getUser();
   const user = {
     name: empData.txFullName,
     email: supabaseUser.email || "example@mail.com",
     avatar: supabaseUser.user_metadata.profile_picture,
+    boHasAdminAccess: empData.boHasAdminAccess,
   };
 
   let sidebarData = userSidebarData;
@@ -120,8 +152,20 @@ export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sideb
             <SidebarMenuButton size="lg" asChild>
               <Link href="/dashboard">
                 <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-9 items-center justify-center rounded-lg">
-                  <Image src="/images/logolight-removedbg.jpg" alt="logo light mode" width={150} height={150} className="xl:w-200 object-contain dark:hidden" />
-                  <Image src="/images/logodark-removedbg.jpg" alt="logo dark mode" width={150} height={150} className="xl:w-200 object-contain hidden dark:block " />
+                  <Image
+                    src="/images/logolight-removedbg.jpg"
+                    alt="logo light mode"
+                    width={150}
+                    height={150}
+                    className="xl:w-200 object-contain dark:hidden"
+                  />
+                  <Image
+                    src="/images/logodark-removedbg.jpg"
+                    alt="logo dark mode"
+                    width={150}
+                    height={150}
+                    className="xl:w-200 object-contain hidden dark:block "
+                  />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-extrabold text-lg">Izifinance</span>
@@ -136,7 +180,11 @@ export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sideb
         <SidebarGroup>
           <SidebarMenu>
             {sidebarData.navMain.map((item, index) => (
-              <Collapsible key={item.title} defaultOpen={index === 1} className="group/collapsible">
+              <Collapsible
+                key={item.title}
+                defaultOpen={index === 1}
+                className="group/collapsible"
+              >
                 <SidebarMenuItem>
                   <CollapsibleTrigger asChild>
                     <SidebarMenuButton>
@@ -153,7 +201,9 @@ export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sideb
                           <SidebarMenuSubItem key={item.title}>
                             <SidebarMenuSubButton asChild>
                               <Link href={item.url}>
-                                {item.icon && <item.icon className="mr-2 h-4 w-4" />}
+                                {item.icon && (
+                                  <item.icon className="mr-2 h-4 w-4" />
+                                )}
                                 {item.title}
                               </Link>
                             </SidebarMenuSubButton>
