4e0cbda237e8760fbe2a94c0516a4f4881e10703
testboiz <tvmini0202@gmail.com>
2025-09-12 22:08:44 +0700

    Revert "fix : fix lint  errors and transitive errors"
    This reverts commit 60bb462ad5103b7ecea40e6d6c44d29556976ad4.

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,37 +1,8 @@
 import * as React from "react";
-import {
-  FilePlus,
-  FolderCog,
-  IdCardLanyard,
-  ListPlus,
-  Minus,
-  Plus,
-  ReceiptText,
-  Tag,
-  UserPen,
-  UserStar,
-  Wallet,
-} from "lucide-react";
+import { FilePlus, FolderCog, IdCardLanyard, ListPlus, Minus, Plus, ReceiptText, Tag, UserPen, UserStar, Wallet } from "lucide-react";
 
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-  SidebarRail,
-} from "@/components/ui/sidebar";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem, SidebarRail } from "@/components/ui/sidebar";
 import Link from "next/link";
 import Image from "next/image";
 import { getEmpInfo, getUser } from "@/queries/server-queries";
@@ -127,16 +98,13 @@ const userSidebarData = {
   ],
 };
 
-export async function AppSidebar({
-  ...props
-}: React.ComponentProps<typeof Sidebar>) {
+export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const empData = await getEmpInfo();
   const supabaseUser = await getUser();
   const user = {
     name: empData.txFullName,
     email: supabaseUser.email || "example@mail.com",
     avatar: supabaseUser.user_metadata.profile_picture,
-    boHasAdminAccess: empData.boHasAdminAccess,
   };
 
   let sidebarData = userSidebarData;
@@ -152,20 +120,8 @@ export async function AppSidebar({
             <SidebarMenuButton size="lg" asChild>
               <Link href="/dashboard">
                 <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-9 items-center justify-center rounded-lg">
-                  <Image
-                    src="/images/logolight-removedbg.jpg"
-                    alt="logo light mode"
-                    width={150}
-                    height={150}
-                    className="xl:w-200 object-contain dark:hidden"
-                  />
-                  <Image
-                    src="/images/logodark-removedbg.jpg"
-                    alt="logo dark mode"
-                    width={150}
-                    height={150}
-                    className="xl:w-200 object-contain hidden dark:block "
-                  />
+                  <Image src="/images/logolight-removedbg.jpg" alt="logo light mode" width={150} height={150} className="xl:w-200 object-contain dark:hidden" />
+                  <Image src="/images/logodark-removedbg.jpg" alt="logo dark mode" width={150} height={150} className="xl:w-200 object-contain hidden dark:block " />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-extrabold text-lg">Izifinance</span>
@@ -182,11 +138,7 @@ export async function AppSidebar({
         <SidebarGroup>
           <SidebarMenu>
             {sidebarData.navMain.map((item, index) => (
-              <Collapsible
-                key={item.title}
-                defaultOpen={index === 1}
-                className="group/collapsible"
-              >
+              <Collapsible key={item.title} defaultOpen={index === 1} className="group/collapsible">
                 <SidebarMenuItem>
                   <CollapsibleTrigger asChild>
                     <SidebarMenuButton>
@@ -203,9 +155,7 @@ export async function AppSidebar({
                           <SidebarMenuSubItem key={item.title}>
                             <SidebarMenuSubButton asChild>
                               <Link href={item.url}>
-                                {item.icon && (
-                                  <item.icon className="mr-2 h-4 w-4" />
-                                )}
+                                {item.icon && <item.icon className="mr-2 h-4 w-4" />}
                                 {item.title}
                               </Link>
                             </SidebarMenuSubButton>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-// import { defineConfig } from "vite";
-// import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// export default defineConfig({
-//   plugins: [tsconfigPaths()],
-//   test: {
-//     globals: true,
-//     environment: "node",
-//   },
-// });
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "node",
+  },
+});
