f7d4040c01ae7598197aa77ed89942c6b2848dd4
testboiz <tvmini0202@gmail.com>
2025-09-17 21:41:42 +0700

    todo (fix) : add action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -171,6 +171,7 @@ export default function Home() {
                       className="flex flex-col gap-4 "
                       onSubmit={form.handleSubmit(onSubmit)}
                       method="POST"
+                      action="/api/v1/auth/signin"
                     >
                       <div className="flex flex-row items-center gap-2">
                         <FormField
